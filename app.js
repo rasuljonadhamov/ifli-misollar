ce4d079b09fe9783f2c9eddd4dc77756fcab9537
rasuljonadhamov <rasuljonadhamov@gmail.com>
2023-09-19 09:28:37 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+// BMI colculator
+// const height = prompt("Bo'yingizni kiriting");
+// const weight = prompt("Vazningizni kiriting");
+
+// const bmi = weight / (height * height);
+
+// if (bmi <= 18.4) {
+//   alert(`Sizning vazningiz normadan kam ${bmi}`);
+// } else if (bmi > 18.5 && bmi <= 24.9) {
+//   alert(`Sizning vazningiz normal ${bmi}`);
+// } else if (bmi >= 25 && bmi <= 39.9) {
+//   alert(`Sizning vazningiz overweight ${bmi}`);
+// } else {
+//   alert(`Sizning vazningiz normadan yuqori ${bmi}`);
+// }
+
+// 1 masala
+// const son1 = +prompt("Son kiriting");
+
+// if (son1 > 0) {
+//   alert(`Son ${son1 + 1}`);
+// } else {
+//   alert(`Siz manfiy son kiritdingiz ${son1}`);
+// }
+
+// 2 masala
+// const son2 = +prompt("Son kiriting");
+
+// if (son2 > 0) {
+//   alert(`Son ${son2 + 1}`);
+// } else {
+//   alert(`Siz manfiy son kiritdingiz ${son2 - 2}`);
+// }
+
+// 3 masala
+// const son3 = +prompt("Son kiriting");
+
+// if (son3 > 0) {
+//   alert(`Son ${son3 + 1}`);
+// } else if (son3 === 0) {
+//   alert(`Son ${10}`);
+// } else {
+//   alert(`Siz manfiy son kiritdingiz ${son3 - 2}`);
+// }
